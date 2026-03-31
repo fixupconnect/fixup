12ae6a3c268ed6f915bbf8303d27b29059a4cebb
fixupconnect <fixupconnect@gmail.com>
2026-03-31 17:52:27 +0530

    Add files via upload

---
+++ fixupweb/js/firebase.js
@@ -0,0 +1,12 @@
+// Your Firebase config (replace with your own)
+var firebaseConfig = {
+  apiKey: "AIzaSyCUdcFjtMWBO_x9Wyomi9WYOLBNunw-MR0",
+  authDomain: "fixup-11f9d.firebaseapp.com",
+  projectId: "fixup-11f9d",
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Firestore reference
+var db = firebase.firestore();
